b6daeacc15cf53a53b40d5a25a50c9ae8cb81735
Michael Cleverdon <michaelcleverdon@u.boisestate.edu>
2018-10-19 08:38:38 -0600

    Init Commit

---
+++ catalog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.urls import path
+from django.views.generic import RedirectView
+
+from catalog import views
+
+urlpatterns = [
+    path('library/', views.library, name='library'),
+
+    url(r'^favicon\.ico$', RedirectView.as_view(url='static/images/favicon.ico')),
+    path('', views.index, name='index'),
+
+]
