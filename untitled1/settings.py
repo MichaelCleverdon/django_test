1d8617fca7d88447090c217e1bd8db1d9ff8a841
Michael Cleverdon <michaelcleverdon@u.boisestate.edu>
2018-10-31 08:57:13 -0600

    Removed django_heroku import from settings.py

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,4 +122,4 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
